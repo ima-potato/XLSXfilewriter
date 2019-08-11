6154679fcf651369e4e77bb101fafc9ab5c05230
ima-potato <aviv.guese.26@gmail.com>
2019-08-11 12:42:58 +0100

    "initial commit"

--- src/main/java/com/lilo/liloproject/util/TimesheetUtils.java
+++ src/main/java/com/lilo/liloproject/util/TimesheetUtils.java
@@ -1,33 +1,42 @@
 /**
  * Author: Aviv Guese
- * <p>
- * <p>
- * Utility that formats timelogs and timesheet into an object that is format specific and
- * will be used in the display and download of timesheet
+ *
+ * Utility used in formatting time log objects into client preference format
  **/
 package com.lilo.liloproject.util;
 
+import com.lilo.liloproject.controller.TimesheetController;
+import com.lilo.liloproject.dto.TimeLogDTO;
 import com.lilo.liloproject.dto.TimesheetEntryDTO;
 import com.lilo.liloproject.dto.TimesheetFormatDTO;
 import com.lilo.liloproject.model.TimeLog;
 import com.lilo.liloproject.model.Timesheet;
+import com.lilo.liloproject.repository.TimesheetRepository;
+import com.lilo.liloproject.service.TimesheetServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+/**
+ * Author: Aviv Guese
+ *
+ *
+ *  Utility that formats timelogs and timesheet into an object that is format specific and
+ *  will be used in the display and download of timesheet
+ *
+ *
+ **/
+import java.sql.Time;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
-
-
 
 @Service
 public class TimesheetUtils extends TimesheetElementUtils {
     private List<TimesheetEntryDTO> entryList = new ArrayList<>();
-    private static final Logger log = LoggerFactory.getLogger(TimesheetUtils.class);
+    private static final Logger log = LoggerFactory.getLogger(TimesheetController.class);
 
     public TimesheetFormatDTO timesheetGenerator(Timesheet timesheet, String timezone) {
         if (timesheet == null) {
@@ -36,7 +45,7 @@ public class TimesheetUtils extends TimesheetElementUtils {
     }
 
     public TimesheetFormatDTO timesheetFormatter(Timesheet timesheet, String timezone) {
-        System.out.println(timesheet);
+
         List<TimeLog> timeLogs = timesheet.getTimeLogs();
         ZonedDateTime tsStartDate = timesheet.getStartDateTime().withZoneSameInstant(ZoneId.of(timezone));
         ZonedDateTime tsEndDate = timesheet.getEndDateTime().withZoneSameInstant(ZoneId.of(timezone));
@@ -55,7 +64,6 @@ public class TimesheetUtils extends TimesheetElementUtils {
         TimesheetFormatDTO formattedTimesheet = new TimesheetFormatDTO();
         formattedTimesheet.setStartDateTime(tsStartDate);
         formattedTimesheet.setEndDateTime(tsEndDate);
-
         if (timesheetStartDate == 16) {
             timesheetEndDate = initialLog.toLocalDate().lengthOfMonth();
 
@@ -70,21 +78,6 @@ public class TimesheetUtils extends TimesheetElementUtils {
         }
 
         for (int i = 0; i < timesheet.getTimeLogs().size(); i++) {
-
-            String timeIn = "";
-            String timeOut = "";
-
-            TimeLog timeLog = timesheet.getTimeLogs().get(i);
-            //add the log if next log is not the succeeding day
-                if (!timeLog.getTimeIn().toString().equals("")) {
-                    timeIn = timeLog.getTimeIn().withZoneSameInstant(ZoneId.of(timezone)).toLocalDateTime().toString();
-                }
-                if (!timeLog.getTimeOut().toString().equals("")) {
-                    timeOut = timeLog.getTimeOut().withZoneSameInstant(ZoneId.of(timezone)).toLocalDateTime().toString();
-                }
-
-
-
             Boolean isHoliday = false;
             Boolean isWeekend = false;
             Boolean hasNighshift = false;
@@ -94,10 +87,26 @@ public class TimesheetUtils extends TimesheetElementUtils {
             Double weekendHours = 0.0;
             Double overtimeHours = 0.0;
             int sandwichedEntryCounter = 0;
-            int currentLogDayValue = timeLog.getTimeIn().withZoneSameInstant(ZoneId.of(timezone)).getDayOfMonth();
-
             int firstCutOffEnd = 15;
+
+            String timeIn = "";
+            String timeOut = "";
+
+            TimeLog timeLog = timesheet.getTimeLogs().get(i);
+            //add the log if next log is not the succeeding day
+            try {
+                if (!timeLog.getTimeIn().toString().equals("")) {
+                    timeIn = timeLog.getTimeIn().withZoneSameInstant(ZoneId.of(timezone)).toLocalDateTime().toString();
+                }
+                if (!timeLog.getTimeOut().toString().equals("")) {
+                    timeOut = timeLog.getTimeOut().withZoneSameInstant(ZoneId.of(timezone)).toLocalDateTime().toString();
+                }
+            }catch(NullPointerException e){
+
+            }
+            int currentLogDayValue = timeLog.getTimeIn().withZoneSameInstant(ZoneId.of(timezone)).getDayOfMonth();
             int secondCutOffEnd = timeLog.getTimeIn().getMonth().maxLength();
+
 
             //end of the month filler
 
@@ -125,6 +134,7 @@ public class TimesheetUtils extends TimesheetElementUtils {
             }
         }
 
+
         formattedTimesheet.setEntryList(entryList);
         return formattedTimesheet;
     }
@@ -140,14 +150,14 @@ public class TimesheetUtils extends TimesheetElementUtils {
         int timesheetEndDate = 15;
         int timesheetCutOffStartDate = 1;
         if (LocalDate.now().getDayOfMonth() >= 16) {
-            System.out.println(LocalDateTime.now().getDayOfMonth() + "is greater than 16");
+
             offsetDay = 16;
             timesheetEndDate = LocalDate.now().getMonth().maxLength();
             timesheetCutOffStartDate = 16;
         }
 
         for (int i = offsetDay; i <= timesheetEndDate-1; i++) {
-            System.out.println("month value now is " + LocalDateTime.now().getMonthValue());
+
 //            int monthNumber = LocalDateTime.now().getMonthValue();
             int monthNumber = ZonedDateTime.now().withZoneSameInstant(ZoneId.of(timezone)).toLocalDate().getMonthValue();
             String placeHolderLog = "2019-" + String.format("%02d", monthNumber) + "-" +
